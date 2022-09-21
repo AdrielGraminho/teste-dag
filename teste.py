48406873ac04ae22813e5f6bd2d5c351a7ade819
Adriel Graminho <69993141+AdrielGraminho@users.noreply.github.com>
2022-09-21 15:04:05 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,11 +1,43 @@
-from airflow.operators.dummy import DummyOperator
-from airflow.operators.bash import BashOperator
-# The DummyOperator is a task and does nothing   
-accurate = DummyOperator(
-task_id='accurate'
-)
-# The BashOperator is a task to execute a bash command
-commands = BashOperator(
-task_id='commands'
-bash_command='sleep 5'
-)
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.bash_operator import BashOperator
+
+
+# In[3]:
+
+
+default_args = {
+   'owner': 'admin',
+   'depends_on_past': False,
+   'start_date': datetime(2022, 9, 16),
+   'retries': 0,
+   }
+
+
+# In[6]:
+
+
+with DAG(
+    'my-first-dag',
+    schedule_interval=timedelta(minutes=1),
+    catchup=False,
+    default_args=default_args
+) as dag:
+    t1 = BashOperator(
+        task_id='first_etl',
+        bash_command="""
+        echo "hello"
+        """)
+    t2 = BashOperator(
+        task_id='second_etl',
+        bash_command="""
+        echo "hello 2"
+        """)
+    t1 >> t2
+
